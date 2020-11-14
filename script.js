382d20cd5a88fcbecd533d7795dcc0594bde8e04
jesseyproctor <jss.proctor@gmail.com>
2020-11-14 16:01:18 -0500

    step-6-validations

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+console.log('Add validation!');
+
+
 
 const form = document.querySelector('#parking-form')
 
@@ -7,6 +10,7 @@ form.addEventListener('submit', function (event) {
     validate()
 })
 
+
 function totalCost() {
     let numDays = document.querySelector('#days').value
     let result = eval(numDays * 5)
@@ -14,3 +18,30 @@ function totalCost() {
     display.innerHTML = result
 }
 
+
+
+function validate() {
+    let creditCardNum = document.querySelector('#credit-card').value 
+    validateCardNumber(creditCardNum)
+
+function validateCardNumber(number) {
+    var regex = new RegExp("^[0-9]{16}$");
+    if (!regex.test(number))
+        return false;
+
+    return luhnCheck(number);
+}
+function luhnCheck(val) {
+    var sum = 0;
+    for (var i = 0; i < val.length; i++) {
+        var intVal = parseInt(val.substr(i, 1));
+        if (i % 2 == 0) {
+            intVal *= 2;
+            if (intVal > 9) {
+                intVal = 1 + (intVal % 10);
+            }
+        }
+        sum += intVal;
+    }
+    return (sum % 10) == 0;
+}
