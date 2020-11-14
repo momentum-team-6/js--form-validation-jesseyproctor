c6e5f322b7a2ee91d622fc9f0aa71598eab4812a
jesseyproctor <jss.proctor@gmail.com>
2020-11-14 15:30:05 -0500

    step=4=validations

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('Add validation!');
+
+const form = document.querySelector('#parking-form')
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault
+    totalCost()
+    validate()
+})
+
+function totalCost() {
+    let numDays = document.querySelector('#days').value
+    let result = eval(numDays * 5)
+    const display = document.querySelector('#total')
+    display.innerHTML = result
+}
+
